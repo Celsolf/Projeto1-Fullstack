3b1f2ab2d1b14893d11d1596f8d222a16f9a39e0
Celsolf <71788045+Celsolf@users.noreply.github.com>
2024-11-13 17:05:23 -0300

    Search Bar Component creation

---
+++ playlist/src/components/SearchBar.js
@@ -0,0 +1,24 @@
+
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Digite o nome do artista ou álbum"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+    </div>
+  );
+};
+
+export default SearchBar;
