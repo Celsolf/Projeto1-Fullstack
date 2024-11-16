0e20a0f57dbc27fbfb9fe76558c33277cfbed1ca
Celsolf <71788045+Celsolf@users.noreply.github.com>
2024-11-16 19:16:15 -0300

    border for list

--- playlist/src/components/AlbumList.js
+++ playlist/src/components/AlbumList.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const AlbumList = ({ albums, onSelectAlbum }) => {
   return (
     <div>
-      <h2>Resultados da Busca</h2>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Resultados da Busca
+      </Typography>
       {albums.map((album) => (
-        <div key={album.collectionId} onClick={() => onSelectAlbum(album)}>
-          <img src={album.artworkUrl100} alt={album.collectionName} />
-          <p>{album.collectionName}</p>
-          <p>{album.artistName}</p>
-        </div>
+        <Box
+          key={album.collectionId}
+          onClick={() => onSelectAlbum(album)}
+          border={1}
+          borderRadius={2} 
+          padding={2}
+          marginBottom={2} 
+          borderColor="grey.300"
+          display="flex"
+          alignItems="center"
+          style={{ cursor: 'pointer' }}
+        >
+          <img src={album.artworkUrl100} alt={album.collectionName} style={{ marginRight: '10px', borderRadius: '4px' }} />
+          <div>
+            <Typography variant="h6">{album.collectionName}</Typography>
+            <Typography color="textSecondary">{album.artistName}</Typography>
+          </div>
+        </Box>
       ))}
     </div>
   );
